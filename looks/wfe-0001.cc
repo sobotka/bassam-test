13e2ceea7ff242805e3913dc565b45a92501de5e
Troy James Sobotka <troy.sobotka@gmail.com>
2016-03-06 00:03:18 -0800

    Basic proof of principle of per shot grades. To use this layout, name your shots such that they align with the per shot grades naming convention. For example, this demonstration uses wfe-####.cc, where the #### is the shot number.
    Just before you load Blender, export the SHOT variable and set it
    to either 0001 or 0002, which are both CDL defined colour corrections.
    
    Now when you load Blender, the Bassam Shot will automatically redirect
    to the given per shot grade, allowing you to start immediately working
    without any changes.

---
+++ looks/wfe-0001.cc
@@ -0,0 +1,10 @@
+<ColorCorrection id="mygrade">
+        <SOPNode>
+             <Slope>2 1 1</Slope>
+             <Offset>0 0 0</Offset>
+             <Power>1 1 1</Power>
+        </SOPNode>
+        <SATNode>
+             <Saturation>1</Saturation>
+	</SATNode>
+</ColorCorrection>
---
+++ looks/wfe-0002.cc
@@ -0,0 +1,10 @@
+<ColorCorrection id="mygrade">
+        <SOPNode>
+             <Slope>0.207 0.207 0.207</Slope>
+             <Offset>0 0 0</Offset>
+             <Power>1.610 1.610 1.610</Power>
+        </SOPNode>
+        <SATNode>
+             <Saturation>1</Saturation>
+	</SATNode>
+</ColorCorrection>
